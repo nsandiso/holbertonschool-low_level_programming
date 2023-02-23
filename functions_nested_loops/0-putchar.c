5c1e0466e13f0d30de7ce82e29aa79b873bea90e
nsandiso <nsandiso@gmail.com>
2023-02-23 01:49:02 -0800

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "main.h"
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
 */
 int main(void)
-{   _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');   return (0);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+       	_putchar('\n');	return (0);
 }
