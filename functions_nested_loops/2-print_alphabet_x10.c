26f30566e504b4117be29c4163ed69771d95db2e
nsandiso <nsandiso@gmail.com>
2023-02-23 00:45:44 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,34 @@
-#include "main.h"
+#include "holberton.h"
+
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - prints lower case alphabet
  *
- * Return: Always 0.
- */
-int main(void)
+ **/
+
+void print_alphabet(void)
 {
-	print_alphabet_x10();
-	return (0);
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - prints aphabet 10 times
+ *
+ **/
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		print_alphabet();
+	}
 }
