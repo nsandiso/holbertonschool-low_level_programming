8690b5ff175b61fbc8152f9836972df7cd0b6487
sandiso <ndlamini@singuler.co.za>
2023-03-02 01:39:25 -0800

    4-strpbrk.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to search
- * @accept: set of bytes to search for
- * Description: retruns first occurrence in a string
- * Return: pointer to first instance of string
-**/
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *p;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
 			{
-				return (&s[i]);
+				p = &s[i];
+				return (p);
 			}
+			j++;
 		}
+		i++;
 	}
-	return (NULL);
+
+	return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+char *_strpbrk(char *s, char *accept);
