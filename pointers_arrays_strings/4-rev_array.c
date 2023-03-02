c7957309d7c64040a6764ff9fc7327ab587bb438
sandiso <ndlamini@singuler.co.za>
2023-03-02 01:36:12 -0800

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,26 @@
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * _strpbrk - searchs a string for any of a set of bytes
- *
- * @s: the string to search
- * @accept: the bytes to search for
- *
- * Return: a pointer to the byte in @s that matches one of the bytes in
- * @accept, or NULL if no such byte is found.
- */
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: set of bytes to search for
+ * Description: retruns first occurrence in a string
+ * Return: pointer to first instance of string
+**/
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i, j;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i]; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accept[i])
-				return (s);
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
 		}
-		s++;
 	}
 	return (NULL);
 }
