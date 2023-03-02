58caa1d9b735ed51969e241f2a591e5efd433293
sandiso <ndlamini@singuler.co.za>
2023-03-02 00:02:29 -0800

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,12 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies string src to buffer pointed to by dest
- * @dest: string pointer
- * @src: buffer pointer
- * Description: cpies string src to buffer
- * Return: dest pointer
- **/
-
-char *_strcpy(char *dest, char *src)
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
+void reset_to_98(int *n)
 {
-	int i;
-
-	i = 0;
-
-	while (i >= 0)
-	{
-		dest[i] = src[i];
-
-		if (src[i] == '\0')
-			return (dest);
-		i++;
-	}
-	return (dest);
+	*n = 98;
 }
