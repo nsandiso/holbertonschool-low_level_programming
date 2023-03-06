2b22feb08c49b72f7fa3f25505708e176d03324d
sandiso <ndlamini@singuler.co.za>
2023-03-06 04:39:21 -0800

    4-strpbrk.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 int _putchar(char c);
 char *_strchr(char *s, char c);
-char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
-char *_strpbrk(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
