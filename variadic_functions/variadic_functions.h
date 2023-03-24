bc8a530c9b16ed4562a9a5a9beef29aed20a8722
sandiso <ndlamini@singuler.co.za>
2023-03-24 07:28:17 -0700

    re-do

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,34 @@
-int _putchar(char c);
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Auth: Tshepo Monene
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
+ */
+
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
+
+/**
+ * struct checker - A new struct type defining a checker.
+ * @type: A type representing a data type.
+ * @f: A function pointer to a function that check
+ *         a data type corresponding to type.
+ */
+
+typedef struct checker
+{
+char *type;
+void (*f)();
+} checker;
+#endif
